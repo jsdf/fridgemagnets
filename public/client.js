b85e74a9d9ea537f0d00bfb7ccbc8819100c5a8d
James Friend <james@dhmedia.com.au>
2013-01-25 10:18:55 +0800

    initial commit

--- public/client.js
+++ public/client.js
@@ -1,4 +1,17 @@
 $(function() {
+
+  var alphabet = ['e', 'e', 'e', ':', ')', 'a', 'a', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  $.each(alphabet, function(index, value) {
+    var letter = $('<span></span>')
+      .text(value)
+      .addClass('magnet')
+      .attr('id','magnet'+index)
+      .css('color', tinycolor({ h: Math.random()*360, s: 90, l: 60 }).toHexString())
+      // .css('color', $.husl.toHex(Math.random()*360, 100, 90))
+      .appendTo(document.body);
+  })
+
   var client = new Faye.Client('/faye');
 
   client.subscribe('/magnets', function(magnets) {
@@ -15,5 +28,5 @@ $(function() {
       magnets[ui.helper.attr('id')] = { position: ui.position };
       client.publish('/magnets', magnets);
     })
-    .droppable()
+    .droppable();
 });
